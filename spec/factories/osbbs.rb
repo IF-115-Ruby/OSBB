89abc18d8363b4b646064d17de6918be3861b827
Mykhailo Marusyk <misha.marusyk@gmail.com>
2020-10-01 18:53:37 +0300

    Add gems dotenv and annotate

--- spec/factories/osbbs.rb
+++ spec/factories/osbbs.rb
@@ -7,3 +7,21 @@ FactoryBot.define do
     is_available { true }
   end
 end
+
+# == Schema Information
+#
+# Table name: osbbs
+#
+#  id           :bigint           not null, primary key
+#  email        :string
+#  is_available :boolean
+#  name         :string
+#  phone        :string
+#  website      :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_osbbs_on_name  (name)
+#
--- spec/models/osbb_spec.rb
+++ spec/models/osbb_spec.rb
@@ -66,3 +66,21 @@ RSpec.describe Osbb, type: :model do
     end
   end
 end
+
+# == Schema Information
+#
+# Table name: osbbs
+#
+#  id           :bigint           not null, primary key
+#  email        :string
+#  is_available :boolean
+#  name         :string
+#  phone        :string
+#  website      :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_osbbs_on_name  (name)
+#
