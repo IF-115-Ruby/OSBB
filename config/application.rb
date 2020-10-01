a37691f912264384638b898ede07a9112379c3be
Axedez <45712870+Axedez@users.noreply.github.com>
2020-10-01 10:30:22 +0300

    hot fix #2

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module OSBB
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.exceptions_app = self.routes
-    config.secret_key_base = 'admin'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/404", :to => "errors#not_found"
-  get "/422", :to => "errors#unacceptable"
-  get "/500", :to => "errors#server_error"
+  get "/404", to: "errors#not_found"
+  get "/422", to: "errors#unacceptable"
+  get "/500", to: "errors#server_error"
 end
