01bf53b392e6c0edf5c17c942eeeedec8a5b38a8
Andrii-Kozak <46384633+Andrii-Kozak@users.noreply.github.com>
2020-10-05 09:10:28 +0300

    Fix user limits (#17)
    * fix limit user
    
    * fix bug
    
    * fix downcase action
    
    * fix downcase email
    
    * add test for downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, length: { maximum: 255 }
 
-  validates :first_name, :last_name, :email, presence: true
-  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  before_save :downcase_email
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase!
   end
 end
 
@@ -14,9 +21,9 @@ end
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  email      :string
-#  first_name :string
-#  last_name  :string
+#  email      :string(254)      not null
+#  first_name :string(50)       not null
+#  last_name  :string(50)       not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,48 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe "#downcase_email" do
+    it "downcases an email before saving" do
+      user.email = "EXEMPLE@GMAIL.COM"
+      user.save
+      expect(user.email).to eq("exemple@gmail.com")
+    end
+  end
+
+  describe "length is invalid" do
+    it "does not allow a first name longer than 50 characters" do
+      user.first_name = "a" * 51
+      expect(user).not_to be_valid
+    end
+
+    it "does not allow a last name longer than 50 characters" do
+      user.last_name = "a" * 51
+      expect(user).not_to be_valid
+    end
+
+    it "does not allow a email longer than 50 characters" do
+      user.email = "a" * 256
+      expect(user).not_to be_valid
+    end
+  end
+
+  describe "length is valid" do
+    it "allow a first name no longer than 50 characters" do
+      user.first_name = "a" * 49
+      expect(user).to be_valid
+    end
+
+    it "allow a last name no longer than 50 characters" do
+      user.last_name = "a" * 49
+      expect(user).to be_valid
+    end
+
+    it "allow a email no longer than 254 characters" do
+      user.email = "#{('a' * 245)}@gmail.com"
+      expect(user).to be_valid
+    end
+  end
+
   describe "validations" do
     context "when email is valid" do
       it "user is valid" do
@@ -37,9 +79,9 @@ end
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  email      :string
-#  first_name :string
-#  last_name  :string
+#  email      :string(254)      not null
+#  first_name :string(50)       not null
+#  last_name  :string(50)       not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
