911771e9a10dd524caaf78defedcdd89af84418e
Andrii Lebukhorskyi <andrey.lebuhorskyy@gmail.com>
2020-10-12 17:06:50 +0300

    Test for rendering view about

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
+  before { get :about }
+
   describe '#index' do
     subject { get :index }
 
     it { is_expected.to be_successful }
+  end
+
+  describe 'render view #about' do
+    it { is_expected.to render_template(:about) }
   end
 end
