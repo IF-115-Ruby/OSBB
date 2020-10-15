9995c47162abdafc8d2af111f4a868fd0875d312
Swooty69 <62611576+Swooty18@users.noreply.github.com>
2020-10-15 13:29:08 +0300

    add change password form (#33)
    * add change password
    
    * review changes
    
    * review changes
    
    * rubocop
    
    * add Internationalization
    
    * review changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name sex mobile])
   end
 
   add_flash_types :danger, :info, :warning, :success
