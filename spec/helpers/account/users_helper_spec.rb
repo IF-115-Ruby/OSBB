a890595be53a682602d65da4adaf079eeda382ec
ukhanskyi <ukhanskyi@gmail.com>
2020-10-20 10:06:39 +0300

    View log_in, sign_up, forgot_password and change_password pages with i18n. (#37)
    Fix view for mobile and fix remark...

--- spec/helpers/account/users_helper_spec.rb
+++ spec/helpers/account/users_helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Account::UsersHelper do
   describe '#options_for_sex' do
     it 'returns options with translations' do
-      expected_result = [%w[Male male], %w[Female female], ['not set', 'no_sex']]
+      expected_result = [%w[Male male], %w[Female female], ['Not set', 'no_sex']]
       expect(helper.options_for_sex).to eq(expected_result)
     end
   end
