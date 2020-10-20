6aa3d1f5f3ec7de1f29643071cf47597f4315281
Vasyl <matsevko.vasil@gmail.com>
2020-10-20 16:39:24 +0300

    added and adjusted gem pretender

--- app/controllers/account/admin/admin_controller.rb
+++ app/controllers/account/admin/admin_controller.rb
@@ -1,2 +1,17 @@
 class Account::Admin::AdminController < Account::AccountController
+  def start_impersonate
+    user = User.non_admin.find_by(id: params[:user_id])
+    if user.present?
+      impersonate_user(user)
+      redirect_to home_path
+    else
+      flash.now[:danger] = 'You can\'t impersonate this user'
+      redirect_to account_admin_users_path
+    end
+  end
+
+  def stop_impersonating
+    stop_impersonating_user
+    redirect_to account_admin_users_path
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require 'support/controller_macros'
 require 'telegram/bot/rspec/integration/rails'
+require 'fileutils'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -24,6 +25,7 @@ require 'telegram/bot/rspec/integration/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
+
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
