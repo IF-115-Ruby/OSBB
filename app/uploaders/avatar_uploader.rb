b71e25e31dc090464b1819d4c06167b9e49d04a6
Andrii Kozak <kozakdocs@gmail.com>
2020-10-22 20:41:26 +0300

    fix user avatar size

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -34,9 +34,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :user_index do
+    process resize_to_fill: [100, 100]
+  end
+
+  version :user_show do
+    process resize_to_fill: [150, 150]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
 
