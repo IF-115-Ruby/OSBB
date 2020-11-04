4aa9d3000e9beefb29f9f5bb016213bb9281c4a4
Ivan <ivan.matsiyk18092002@gmail.com>
2020-11-04 23:29:20 +0200

    delete comments

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -2,11 +2,7 @@
 
 class User::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-
-  # POST /resource
   def create
     if params[:flag]
       build_resource(sign_up_params)
