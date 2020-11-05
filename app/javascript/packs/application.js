24d57a22894e6f0f5f5c8384c507b79f352b4ea8
DenisRem <ihor.moskvun5@gmail.com>
2020-11-06 00:10:42 +0200

    Add React fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
+import "bootstrap";
+import "../stylesheets/application";
+import "./custom";
+import "@fortawesome/fontawesome-free/js/all";
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -8,7 +12,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("../packs/reactrailsujs")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,11 +21,3 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "bootstrap";
-import "../stylesheets/application";
-import "./custom";
-import "@fortawesome/fontawesome-free/js/all";
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
