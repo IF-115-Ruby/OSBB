7e277dfcb73e5b78ee917401c4404c6f89230afa
Ivan <ivan.matsiyk18092002@gmail.com>
2020-11-05 12:45:43 +0200

    second conflict

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -5,93 +5,40 @@ class User::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    super
-  end
 
   # POST /resource
   def create
-    build_resource(sign_up_params)
-    @osbb = Osbb.create(name: params[:user][:osbb][:name], phone: params[:user][:osbb][:phone], email: params[:user][:osbb][:email], website: params[:user][:osbb][:website])
-    if resource.valid? && @osbb.valid?
-      resource.update(osbb_id: @osbb.id)
-      resource.save
-
-      yield resource if block_given?
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
-        else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+    if params[:flag]
+      build_resource(sign_up_params)
+      @osbb = Osbb.new(name: params[:user][:osbb][:name], phone: params[:user][:osbb][:phone], email: params[:user][:osbb][:email], website: params[:user][:osbb][:website])
+      if @osbb.valid? && resource.valid?
+        @osbb.save
+        resource.update(osbb_id: @osbb.id, role: User::LEAD)
+        yield resource if block_given?
+        if resource.persisted?
+          if resource.active_for_authentication?
+            set_flash_message! :notice, :signed_up
+            sign_up(resource_name, resource)
+            respond_with resource, location: after_sign_up_path_for(resource)
+          else
+            set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+            expire_data_after_sign_in!
+            respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          end
         end
-      end
-    else
-        unless @osbb.save && resource.save
-
-          flash.now[:warning] = 'Invalid OSBB parameters'
-          render :new and return
-        end
+      else
+        render :new
         clean_up_passwords resource
         set_minimum_password_length
+      end
+    else
+      super
     end
-
-    # resource.company_id = @company.id
-    # resource.save
   end
 
   protected
 
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name sex mobile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name sex mobile password])
   end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe User::RegistrationsController, type: :controller do
+  describe "POST #create" do
+    let(:valid_attributes_user) do
+      FactoryBot.build(:user).attributes
+                .merge({ password: '123456', confirm_password: '123456' })
+    end
+    let(:invalid_attributes_user) { FactoryBot.build(:user).attributes }
+    let(:osbb_valid_params) do
+      { name: 'Top OSBB',
+        phone: '1231231231',
+        email: 'osbb@gmail.com',
+        website: 'OsbbTOp.com' }
+    end
+    let(:valid_attributes_osbb) do
+      valid_attributes_user[:osbb] = osbb_valid_params
+      atributes = { user: valid_attributes_user, flag: true }
+      atributes
+    end
+    let(:invalid_attributes_osbb) do
+      valid_attributes_user[:osbb] = { name: 'To' }
+      atributes = { user: valid_attributes_user, flag: true }
+      atributes
+    end
+
+    before do
+      request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+
+    context "with invalid user params" do
+      it "not create a new User" do
+        expect do
+          post :create, params: { user: invalid_attributes_user }
+        end.to change(User, :count).by(0)
+      end
+    end
+
+    context "with valid user params" do
+      it "creates a new User" do
+        expect do
+          post :create, params: { user: valid_attributes_user }
+        end.to change(User, :count).by(1)
+      end
+    end
+
+    context "with valid osbb and user params" do
+      it "creates a new osbb and user" do
+        expect do
+          post :create, params: valid_attributes_osbb
+        end.to change(Osbb, :count).by(1).and change(User, :count).by(1)
+      end
+    end
+
+    context "with invalid osbb and user params" do
+      it "not creates a new osbb and user" do
+        expect do
+          post :create, params: invalid_attributes_osbb
+        end.to change(Osbb, :count).by(0).and change(User, :count).by(0)
+      end
+    end
+  end
+end
