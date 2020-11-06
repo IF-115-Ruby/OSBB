12fb74f510476ba75969aeb9e9e34f561a4ba983
Ivan <ivan.matsiyk18092002@gmail.com>
2020-11-06 16:22:36 +0200

    refactor save_rsource method

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -11,7 +11,7 @@ class User::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  protected
+  private
 
   def render_sign_up
     render :new
@@ -22,14 +22,22 @@ class User::RegistrationsController < Devise::RegistrationsController
   def save_resource
     resource.save
     if resource.active_for_authentication?
-      set_flash_message! :notice, :signed_up
-      sign_up(resource_name, resource)
-      respond_with resource, location: after_sign_up_path_for(resource)
+      active_resource
     else
-      set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-      expire_data_after_sign_in!
-      respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      inactive_resource
     end
+  end
+
+  def active_resource
+    set_flash_message! :notice, :signed_up
+    sign_up(resource_name, resource)
+    respond_with resource, location: after_sign_up_path_for(resource)
+  end
+
+  def inactive_resource
+    set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+    expire_data_after_sign_in!
+    respond_with resource, location: after_inactive_sign_up_path_for(resource)
   end
 
   def osbb_params
