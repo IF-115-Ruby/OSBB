2ea2f9f88b12d1c1a9d44287656b38d63a8f1773
Ivan <ivan.matsiyk18092002@gmail.com>
2020-11-06 16:11:43 +0200

    refactor create method

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -5,29 +5,32 @@ class User::RegistrationsController < Devise::RegistrationsController
     if params[:flag]
       build_resource(user_params)
       resource.osbb_attributes = osbb_params
-      if resource.valid?
-        resource.save
-        yield resource if block_given?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
-        else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        end
-      else
-        render :new
-        clean_up_passwords resource
-        set_minimum_password_length
-      end
+      resource.valid? ? save_resource : render_sign_up
     else
       super
     end
   end
 
   protected
+
+  def render_sign_up
+    render :new
+    clean_up_passwords resource
+    set_minimum_password_length
+  end
+
+  def save_resource
+    resource.save
+    if resource.active_for_authentication?
+      set_flash_message! :notice, :signed_up
+      sign_up(resource_name, resource)
+      respond_with resource, location: after_sign_up_path_for(resource)
+    else
+      set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+      expire_data_after_sign_in!
+      respond_with resource, location: after_inactive_sign_up_path_for(resource)
+    end
+  end
 
   def osbb_params
     params[:user][:osbb_attributes].permit(:name, :phone, :email, :website)
