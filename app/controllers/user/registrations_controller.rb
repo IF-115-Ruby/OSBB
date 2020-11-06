4afe000a620ad9d7a78c6817268bd0b21f486d0f
Ivan <ivan.matsiyk18092002@gmail.com>
2020-11-06 12:22:08 +0200

    code style

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -8,15 +8,15 @@ class User::RegistrationsController < Devise::RegistrationsController
       if resource.valid?
         resource.save
         yield resource if block_given?
-          if resource.active_for_authentication?
-            set_flash_message! :notice, :signed_up
-            sign_up(resource_name, resource)
-            respond_with resource, location: after_sign_up_path_for(resource)
-          else
-            set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-            expire_data_after_sign_in!
-            respond_with resource, location: after_inactive_sign_up_path_for(resource)
-          end
+        if resource.active_for_authentication?
+          set_flash_message! :notice, :signed_up
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
       else
         render :new
         clean_up_passwords resource
