93931a0cd1a4f5622fa87e0a4b8efb7ad8ed377b
Ivan <ivan.matsiyk18092002@gmail.com>
2020-11-09 15:23:55 +0200

    review comments

--- app/workers/sign_up_email_sender_worker.rb
+++ app/workers/sign_up_email_sender_worker.rb
@@ -2,7 +2,9 @@ class SignUpEmailSenderWorker
   include Sidekiq::Worker
 
   def perform(user_id)
-    user = User.find(user_id)
+    user = User.find_by(id: user_id)
+    return unless user
+
     AdminMailer.admin_notification(user).deliver_now
     UserMailer.send_welcome_email(user).deliver_now
   end
