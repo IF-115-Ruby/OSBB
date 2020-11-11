049eb9c31aec01908ee3924efbc578ac85bc76fb
Ivan <ivan.matsiyk18092002@gmail.com>
2020-11-11 16:20:10 +0200

    refactor worker

--- app/workers/calculate_bill_worker.rb
+++ app/workers/calculate_bill_worker.rb
@@ -4,21 +4,27 @@ class CalculateBillWorker
 
   def perform(*_args)
     BillingContract.all.each do |billing_contract|
-      meter_reading = billing_contract.meter_readings.created_between(billing_contract.last_bill.date, Time.now.utc)
-                                      .ordered_by_date.first&.value
-      last_meter_reading = billing_contract.last_bill&.meter_reading
-      delta_meter_reading = meter_reading - last_meter_reading if last_meter_reading && meter_reading
+      delta_meter_reading = get_delta_reading(billing_contract)
       bill_value = calculate_bill(delta_meter_reading, billing_contract)
       bill_value = edit_value_depending_payments(bill_value, billing_contract)
+      byebug
       FactoryBot.create(:bill, amount: bill_value, date: Time.now.utc, billing_contract: billing_contract,
                                meter_reading: meter_reading)
     end
+  end
+
+  def get_delta_reading(billing_contract)
+    meter_reading = billing_contract.meter_readings.created_between(billing_contract.last_bill.date, Time.now.utc)
+                                    .ordered_by_date.first&.value
+    last_meter_reading = billing_contract.last_bill&.meter_reading
+    last_meter_reading ? meter_reading - last_meter_reading : meter_reading
   end
 
   def edit_value_depending_payments(value, contract)
     payments = contract.payments.created_between(contract.last_bill.date, Time.now.utc)
     suma = payments.sum(:amount) || 0
     suma += contract.last_bill.amount
+    byebug
     value + suma
   end
 
