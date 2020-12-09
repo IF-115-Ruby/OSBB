2987c21bc2ae43e22112c946de9b5e968b70fa1e
Ivan <ivan.matsiyk18092002@gmail.com>
2020-12-09 19:18:09 +0200

    refactor naming method

--- app/workers/calculate_bill_worker.rb
+++ app/workers/calculate_bill_worker.rb
@@ -4,15 +4,15 @@ class CalculateBillWorker
 
   def perform(*_args)
     BillingContract.all.each do |billing_contract|
-      meter_reading = get_meter_reading(billing_contract)
-      bill_value = get_bill_value(meter_reading, billing_contract)
+      meter_reading = meter_reading(billing_contract)
+      bill_value = bill_value(meter_reading, billing_contract)
       billing_contract.bills.create(amount: bill_value, date: Time.now.utc, meter_reading: meter_reading)
     end
   end
 
   private
 
-  def get_meter_reading(billing_contract)
+  def meter_reading(billing_contract)
     if billing_contract.last_bill
       billing_contract.meter_readings.created_between(billing_contract.last_bill.date, Time.now.utc)
                       .ordered_by_date.first&.value
@@ -21,18 +21,18 @@ class CalculateBillWorker
     end
   end
 
-  def get_bill_value(meter_reading, billing_contract)
-    delta_meter_reading = get_delta_reading(meter_reading, billing_contract) if meter_reading
+  def bill_value(meter_reading, billing_contract)
+    delta_meter_reading = delta_reading(meter_reading, billing_contract) if meter_reading
     bill_value = calculate_bill(delta_meter_reading, billing_contract)
-    edit_value_depending_payments(bill_value, billing_contract)
+    value_depending_payments(bill_value, billing_contract)
   end
 
-  def get_delta_reading(meter_reading, billing_contract)
+  def delta_reading(meter_reading, billing_contract)
     last_meter_reading = billing_contract.last_bill&.meter_reading
     last_meter_reading ? meter_reading - last_meter_reading : meter_reading
   end
 
-  def edit_value_depending_payments(value, billing_contract)
+  def value_depending_payments(value, billing_contract)
     suma = if billing_contract.last_bill
              billing_contract.payments.created_between(billing_contract.last_bill.date, Time.now.utc).sum(:amount) || 0
            else
